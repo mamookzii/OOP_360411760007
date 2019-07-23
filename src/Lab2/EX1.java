2d95f85fa9ee0b644565a4e5059d435cae8a0781
Naputsorn <mookpakphanang@gmail.com>
2019-07-23 10:41:11 +0700

    Usage Examples Chapter2

---
+++ src/Lab2/EX1.java
@@ -0,0 +1,65 @@
+package Lab2;
+
+public class EX1 {
+    public static void main(String[] args) {
+
+        //data type in java
+        //integer
+        int x; // x is integer
+        x = 10;
+        System.out.println("x = "+x);
+        x = 20;
+        int a,b,c = 30;
+
+        //real number
+        double n = 20.5;
+        float m = 10;
+        n = 20.5;
+        m = 10;
+        System.out.println(n);
+        System.out.println(m);
+
+        //charecters
+        char d = '#';
+        System.out.println(d);
+
+        //sting
+        String s = "mook";
+        System.out.println(s);
+        String l = " phanchai";
+        String fullname = s + l;
+        System.out.println(fullname);
+        System.out.println(fullname.length());
+
+        String t = fullname.concat(" RUTS");
+        System.out.println(t);
+
+        //boolean
+        boolean w = true;
+        boolean o = false;
+        System.out.println(!w);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+}//class
+
